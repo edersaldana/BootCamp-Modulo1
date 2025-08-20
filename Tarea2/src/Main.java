87de6fcef27ac427b8b44720ff90bb6aeb8203d1
edersaldana <eder.11.joel@gmail.com>
2025-08-19 23:11:58 -0500

    feat: Se agrega funcionalidad de agregar datos predeterminados al sistema de biblioteca usando la clase Object.

--- Tarea2/src/Main.java
+++ Tarea2/src/Main.java
@@ -1,9 +1,48 @@
-import Packages.*;
+/**
+ * Autor: Eder Saldaña More
+ * Fecha: 19/08/25
+ * Resumen: Tarea2: Proyecto de Clases, Interfaces, Bucles Arreglos
+ *
+ * EL proyecto es un sistema basico de BIblioteca, el cual podra registrar libros,
+ * revistas o tesis, ademas se puede registar a los diferentes usuarios para el
+ * prestamo de libros.
+ * Cada vez que se empresta un libro, este dejara de estar disponible en el sistema.
+ */
 
+import Packages.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Object[][] books = {
+                {"Cien años de soledad", "Gabriel García Márquez"},
+                {"Don Quijote de la Mancha", "Miguel de Cervantes Saavedra"},
+                {"Orgullo y prejuicio", "Jane Austen"},
+                {"Crimen y castigo", "Fiódor Dostoyevski"},
+                {"Hamlet", "William Shakespeare"},
+                {"La Odisea", "Homero"},
+                {"La Divina Comedia", "Dante Alighieri"},
+                {"Fausto", "Johann Wolfgang von Goethe"},
+                {"La Metamorfosis", "Franz Kafka"}
+        };
+
+        Object[][] magazines = {
+                {"National Geographic", "Gardiner Greene Hubbard"},
+                {"TIME", "Henry Luce y Briton Hadden"},
+                {"Scientific American", "Rufus Porter"},
+                {"Nature", "Norman Lockyer"},
+                {"Forbes", "B.C. Forbes"}
+        };
+
+        Object[][] thesis = {
+                {"Desarrollo de un sistema de gestión de inventario con Java y MySQL", "Juan Pérez Rodríguez"},
+                {"Implementación de una aplicación móvil para el monitoreo de la salud usando Android", "María Fernanda López"},
+                {"Algoritmo de optimización para rutas de transporte público usando Python", "Carlos Sánchez Gómez"},
+                {"Plataforma web para educación virtual basada en Node.js y React", "Luis Alberto Ramírez"},
+                {"Sistema de reconocimiento de voz para asistencia en el hogar con inteligencia artificial", "Daniela Torres Cruz"}
+        };
+
 
         Scanner scan = new Scanner(System.in);
         Library library = new Library();
@@ -17,6 +56,7 @@ public class Main {
             System.out.println("**  Sistema de Préstamo de Materiales  **");
             System.out.println("*****************************************");
             System.out.println(" ");
+            System.out.println("0. Cargar DATA de la Biblioteca");
             System.out.println("1. Registrar libro");
             System.out.println("2. Registrar revista");
             System.out.println("3. Registrar tesis");
@@ -31,6 +71,28 @@ public class Main {
                 option = scan.nextInt();
                 scan.nextLine();
                 switch (option) {
+                    case 0:
+                        // Agregar el repositorio de libros
+                        for (Object[] book : books) {
+                            String title = (String) book[0];
+                            String autor = (String) book[1];
+                            library.addMaterial(new Book("Book", title, autor));
+                        }
+
+                        // Agregar el repositorio de revistas
+                        for (Object[] magazine : magazines) {
+                            String title = (String) magazine[0];
+                            String autor = (String) magazine[1];
+                            library.addMaterial(new Book("Magazine", title, autor));
+                        }
+
+                        // Agregar el repositorio de tesis
+                        for (Object[] thesi : thesis) {
+                            String title = (String) thesi[0];
+                            String autor = (String) thesi[1];
+                            library.addMaterial(new Book("Thesis", title, autor));
+                        }
+                        break;
                     case 1:
                         System.out.print("Título: ");
                         String tLibro = scan.nextLine();
@@ -57,6 +119,7 @@ public class Main {
                         String name = scan.nextLine();
                         System.out.print("Nombre edad: ");
                         int age = scan.nextInt();
+                        scan.nextLine();
                         System.out.print("Nombre ciudad: ");
                         String city = scan.nextLine();
                         library.addUser(new User(name, age, city));
@@ -91,7 +154,6 @@ public class Main {
                 option = -1;
             }
         }
-
         scan.close();
     }
 }
